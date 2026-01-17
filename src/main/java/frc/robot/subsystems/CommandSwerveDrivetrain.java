882dc6345aa2c80d011e9f0bf5c8f6466aea2123
WENDIGO\tadur <shryastaduri@gmail.com>
2026-01-17 12:51:43 -0500

    field pose or something on the smart dashboard

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -145,6 +145,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         if (Utils.isSimulation()) {
             startSimThread();
         }
+        init();
     }
 
     /**
