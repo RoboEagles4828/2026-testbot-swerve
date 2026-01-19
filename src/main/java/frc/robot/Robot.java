cb09b76e0168d4279899c11098d0f0a2c882e8b3
Virus <virupakshnadimpalli@gmail.com>
2026-01-19 15:32:33 -0500

    Theoretical Kraken Kode. PLEASE VALIDATE.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,29 +4,44 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.HootAutoReplay;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     private final RobotContainer m_robotContainer;
-
+    private final TalonFX m_motor;
+    private final CommandXboxController joystick; 
     /* log and replay timestamp and joystick data */
     private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
         .withTimestampReplay()
         .withJoystickReplay();
 
     public Robot() {
+
+        joystick = new CommandXboxController(0);
         m_robotContainer = new RobotContainer();
+        final CANBus kCANBus = CANBus.roboRIO();
+        m_motor = new TalonFX(0, kCANBus);
+
     }
 
     @Override
     public void robotPeriodic() {
         m_timeAndJoystickReplay.update();
+
+        var forward = joystick.getLeftY();
+        final DutyCycleOut m_request = new DutyCycleOut(0.5);
+        m_motor.setControl(m_request.withOutput(forward));
+
         CommandScheduler.getInstance().run(); 
     }
 
